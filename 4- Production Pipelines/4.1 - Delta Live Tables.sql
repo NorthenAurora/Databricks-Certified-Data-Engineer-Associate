7469a9498c8fabf9c27ba0feeda49b6f25e4f360
zulipiya.aman@gmail.com <zulipiya.aman@gmail.com>
2026-01-03 16:44:05 +0000

    continue fixing

--- 4- Production Pipelines/4.1 - Delta Live Tables.sql
+++ 4- Production Pipelines/4.1 - Delta Live Tables.sql
@@ -95,13 +95,10 @@ AS
 
 -- COMMAND ----------
 
-CREATE OR REFRESH LIVE TABLE fr_daily_customer_books
+CREATE OR REFRESH STREAMING LIVE TABLE fr_daily_customer_books
 COMMENT "Daily number of books per customer in France"
-AS
-  SELECT customer_id, f_name, l_name, date_trunc("DD", order_timestamp) order_date, sum(quantity) books_counts
-  FROM LIVE.orders_cleaned
-  WHERE country = "France"
-  GROUP BY customer_id, f_name, l_name, date_trunc("DD", order_timestamp)
+AS SELECT * FROM cloud_files("/Volumes/dev/default/bookstore/france-orders-json-raw", "json",
+                             map("cloudFiles.inferColumnTypes", "true"))
 
 -- COMMAND ----------
 
